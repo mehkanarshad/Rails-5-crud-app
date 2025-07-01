31fdffc047eccd0e8d219a652a11b535b76db42c
mehkanarshad <mehkanarshadskp@gmail.com>
2025-07-01 20:12:01 +0500

    Adding validation for images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   has_one_attached :image
   has_one_attached :file
+  validates :title, presence: true, length: {maximum: 100}
+  validates :body, presence: true
+  # validates :image, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { less_than: 5.megabytes, message: 'is too large' }
+  validates :image , content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: {less_than: 5.megabytes, message: 'is too large'}
 end
