a5bfca67ac6a3510cfae99176d6d2c5170795b7b
mehkanarshad <mehkanarshadskp@gmail.com>
2025-07-01 20:58:22 +0500

    Learned to resize image using the image processing gem

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem "stimulus-rails"
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
+gem 'active_storage_validations'
+gem 'image_processing', '~> 1.2'
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,9 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: {maximum: 100}
   validates :body, presence: true
   # validates :image, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { less_than: 5.megabytes, message: 'is too large' }
-  validates :image , content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: {less_than: 5.megabytes, message: 'is too large'}
+  validates :image , content_type: ['image/png', 'image/jpeg'], size: {less_than: 5.megabytes, message: 'is too large'}
+
+  def thumbnail
+    return self.image.variant(resize_to_limit: [300, 300]).processed if self.image.attached?
+  end
 end
